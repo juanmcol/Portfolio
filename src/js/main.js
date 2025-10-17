4f40b5488f6e51b7b869184a08fe282ee6f65929
juanmcol <jmcolina1996@gmail.com>
2025-10-16 19:05:15 -0500

    Add random quote change. Add scroll for more header and change it to Latest Projects when the user scrolls down

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+// quote array [quote, author]
+const quotes = [
+    ["The true creator is necessity, who is the mother of our invention.", "Plato"],
+    ["Ack Ack", "Martian, Mars Attacks!"]
+];
+
+// change quote randomly
+const getQuote = function() {
+    let quote = document.getElementById("quote");
+    let author = document.getElementById("quote-author");
+
+    let num = 0;
+    do {
+        num = Math.floor(Math.random() * quotes.length);
+    } while (quote.innerHTML == quotes[num][0]);
+
+    quote.innerHTML = quotes[num][0];
+    author.innerHTML = quotes[num][1];
+}
+
+// change quote every x amount of milliseconds
+setInterval(getQuote, 10000);
+
+// change "scroll for more" to "Latest Projects", and hide the arrow
+let latest = document.getElementById("latest-header");
+latest.innerHTML = "scroll down for more";
+
+document.addEventListener("scroll", function (e) {
+    latest.innerHTML = "Latest Projects";
+    document.getElementById("latest").style.padding = "3rem 0";
+    document.getElementById("arrow-div").style.display = "none";
+})
