5b2fd94c83c73c4d3aa8d9e4b9c2bc163221ac67
juanmcol <jmcolina1996@gmail.com>
2025-10-20 22:30:32 -0500

    add better grid layout

--- src/js/main.js
+++ src/js/main.js
@@ -22,11 +22,10 @@ const getQuote = function() {
 setInterval(getQuote, 10000);
 
 // change "scroll for more" to "Latest Projects", and hide the arrow
-let latest = document.getElementById("latest-header");
+let latest = document.getElementById("latest-span");
 latest.innerHTML = "scroll down for more";
 
 document.addEventListener("scroll", function (e) {
     latest.innerHTML = "Latest Projects";
-    document.getElementById("latest").style.padding = "3rem 0";
     document.getElementById("arrow-div").style.display = "none";
 })
